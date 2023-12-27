b4100215d7c9a45f18ca28ed42df7a5129160b4e
ammar <ammaraglan6@gmail.com>
2023-12-27 15:55:24 +0200

    Base

---
+++ src/test/java/com/example/Orders/and/Notifications/Management/OrdersAndNotificationsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Orders.and.Notifications.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersAndNotificationsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
